c86254f300ca214b22cccb280f95254f1c86b339
kobakazu0429 <kobakazu10@gmail.com>
2022-04-17 23:06:56 +0900

    update

--- ninja.sh
+++ ninja.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CLANG_BUILD_DIR="/tmp/clang-build"
-CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS=clang;clang-tools-extra"
+ENABLE_PROJECTS="clang;clang-tools-extra"
 CMAKE_INSTALL_TARGETS="install-clang install-clang-resource-headers install-clang-format"
 CLANG_INSTALL_DIR="/tmp/clang-install"
 
@@ -9,15 +9,18 @@ mkdir -p "$CLANG_BUILD_DIR/build"
 
 pushd "$CLANG_BUILD_DIR/build"
 
-# Run the build as specified in the build arguments.
 echo "Running build"
 
-cmake -GNinja \
-  -DCMAKE_INSTALL_PREFIX="$CLANG_INSTALL_DIR" \
-  $CMAKE_ARGS \
-  "$CLANG_BUILD_DIR/src/llvm"
+set CC=cl
+set CXX=cl
 
-ninja $CMAKE_INSTALL_TARGETS
+cmake -GNinja \
+  "$CLANG_BUILD_DIR/src/llvm" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_ENABLE_PROJECTS="$ENABLE_PROJECTS" \
+  -DCMAKE_INSTALL_PREFIX="$CLANG_INSTALL_DIR" \
+
+mold -run ninja -j$(nproc) $CMAKE_INSTALL_TARGETS
 
 cmake --build "$CLANG_BUILD_DIR/build" --target clangd
 
